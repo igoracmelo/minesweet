1ea332702e7e80100bf6568dc2eaf729750b0af3
Igor Melo <igoracmelo@protonmail.com>
2021-10-29 00:22:17 -0300

    add base game structure

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,74 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="Index">
+    <div class="Game">
+      <div class="Board">
+        <div class="row" v-for="(_, row) of board" :key="row">
+          <Tile
+            v-for="(_, col) of board[row]"
+            :key="col"
+            :row="row"
+            :col="col"
+            :isRevealed="boardRevealed[row][col]"
+            :bombsAround="board[row][col]"
+            @tile-clicked="onTileClicked"
+          />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import Tile from 'components/Tile.vue'
+import { BOMB } from 'src/constants'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  components: {
+    Tile
+  },
+
+  data () {
+    return {
+      board: [],
+      boardRevealed: []
+    }
+  },
+
+  methods: {
+    onTileClicked (pos) {
+      const [row, col] = pos
+      this.boardRevealed[row][col] = true
+    },
+  }
 })
 </script>
+
+<style lang="scss" scoped>
+  .Index {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+  }
+
+  .Game {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .Board {
+    border: 3px outset;
+    padding: 1rem;
+  }
+
+  .row {
+    display: flex;
+  }
+</style>
